1a94801536f3765b3174eee01676ff86f9b5212d
Vyacheslav <venis@difane.com>
2010-08-25 14:17:12 +0300

    Removed a lot of unused code from BasePenlet

--- src/com/difane/games/ticktacktoe/BasePenlet.java
+++ src/com/difane/games/ticktacktoe/BasePenlet.java
@@ -2,25 +2,18 @@ package com.difane.games.ticktacktoe;
 
 import java.util.Vector;
 
-import com.livescribe.afp.PageInstance;
-import com.livescribe.configuration.Config;
 import com.livescribe.display.BrowseList;
 import com.livescribe.display.Display;
 import com.livescribe.display.Graphics;
 import com.livescribe.display.Image;
-import com.livescribe.event.HWRListener;
 import com.livescribe.event.MenuEvent;
 import com.livescribe.event.MenuEventListener;
-import com.livescribe.event.PenTipListener;
-import com.livescribe.icr.ICRContext;
-import com.livescribe.icr.Resource;
 import com.livescribe.penlet.Penlet;
-import com.livescribe.penlet.Region;
 import com.livescribe.ui.ScrollLabel;
 /**
  * This Penlet displays "Hello World!" as text when activated by menu.
  */
-public class BasePenlet extends Penlet implements MenuEventListener, PenTipListener {
+public class BasePenlet extends Penlet implements MenuEventListener {
     
 	/**
 	 * Display, which will show information to the user
@@ -47,10 +40,6 @@ public class BasePenlet extends Penlet implements MenuEventListener, PenTipListe
 	Image image;
 	Graphics graphics;
 	
-    // Configuration key for example configuration reading 
-    private static final String configKey = "CONFIG_DATA";
-    private String configData;
-
     public BasePenlet() {   
     }
 
@@ -104,15 +93,6 @@ public class BasePenlet extends Penlet implements MenuEventListener, PenTipListe
         	this.context.addStrokeListener(this.fsm);
             this.fsm.eventStartApplication();
         }
-        /*
-        this.context.addStrokeListener(this);
-
-        // Prompt the user for text entry
-        this.label.draw(this.context.getResourceBundle().getTextResource("icr.instruction.text"), true);
-        this.display.setCurrent(this.label);
-        
-        
-		context.addPenTipListener(this);*/
     }
     
     /**
@@ -120,8 +100,7 @@ public class BasePenlet extends Penlet implements MenuEventListener, PenTipListe
      */
     public void deactivateApp(int reason) {
         this.logger.info("[PENLET] Penlet Main deactivated.");
-        //this.context.removeStrokeListener(this);        
-		//context.removePenTipListener(this);
+        this.context.removeStrokeListener(this.fsm);        
     }
     
     /**
@@ -165,37 +144,5 @@ public class BasePenlet extends Penlet implements MenuEventListener, PenTipListe
 		}
 		
 		return result;
-	}
-
-	public void penUp(long time, Region region, PageInstance page) {
-	
-	}
-
-	public void penDown(long time, Region region, PageInstance page) {
-	
-	}
-
-	public void singleTap(long time, int x, int y) {
-	
-	}
-
-	public void doubleTap(long time, int x, int y) {
-	
-	}
-	
-	
-	
-	private void exampleConfigurationReading() {
-		// Obtain the label from the configuration file
-		Config config = this.context.getAppConfiguration();
-		try {
-			this.configData = config.getStringValue(configKey);
-			this.logger.info("Configuration value = " + this.configData);
-			this.label.draw(configKey + " = " + this.configData, true);
-		} catch (Exception e) {
-			this.configData = "ERROR";
-			this.logger.info("Failed to obtain value for " + configKey
-					+ " in config.txt");
-		}
 	}
 }
