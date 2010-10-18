17c8df0dd4211c30c6b030e18417a1e2acc2dbde
VEnis <venis@difane.com>
2010-10-18 17:55:10 +0300

    Updated SoundManager component. Added handling additional resources. Added some helper methods.

--- src/com/difane/games/ticktacktoe/GameSoundManager.java
+++ src/com/difane/games/ticktacktoe/GameSoundManager.java
@@ -2,8 +2,7 @@ package com.difane.games.ticktacktoe;
 
 import java.util.Hashtable;
 
-import com.livescribe.i18n.AudibleResource;
-import com.livescribe.i18n.ResourceBundle;
+import com.difane.games.ticktacktoe.exceptions.GameBoardLineException;
 import com.livescribe.i18n.SoundResource;
 import com.livescribe.ui.MediaPlayer;
 
@@ -26,6 +25,14 @@ import com.livescribe.ui.MediaPlayer;
  *  - "How to play the game" - menu item
  *  - "Easy" - menu item
  *  - "Hard" - menu item
+ *  - "Line is to short"
+ *  - "Line is not horizontal"
+ *  - "Line is not vertical"
+ *  - "Line must be at the right of previous one"
+ *  - "Line must be at the bottom of previous one"
+ *  - "Line must be near the other line"
+ *  - "Line must cross both vertical lines"
+ *  - "To start new game start drawing a new board"
  *
  */
 
@@ -77,6 +84,15 @@ public class GameSoundManager {
 		this.resources.put("the-game-ends-with-a-draw", getSoundResourceByName("the-game-ends-with-a-draw"));
 		this.resources.put("tick-tack-toe", getSoundResourceByName("tick-tack-toe"));
 		
+		this.resources.put("line-is-to-short", getSoundResourceByName("line-is-to-short"));
+		this.resources.put("line-must-be-at-the-right", getSoundResourceByName("line-must-be-at-the-right"));
+		this.resources.put("line-must-be-near", getSoundResourceByName("line-must-be-near"));
+		this.resources.put("line-must-cross", getSoundResourceByName("line-must-cross"));
+		this.resources.put("line-is-not-vertical", getSoundResourceByName("line-is-not-vertical"));
+		this.resources.put("line-must-be-at-the-bottom", getSoundResourceByName("line-must-be-at-the-bottom"));
+		this.resources.put("to-start-new-game-start-drawing", getSoundResourceByName("to-start-new-game-start-drawing"));
+		this.resources.put("line-is-not-horizontal", getSoundResourceByName("line-is-not-horizontal"));
+		
 		this.getContainer().getLoggerComponent().debug(
 				"[GameSound] Component initialized");
 	}
@@ -95,7 +111,7 @@ public class GameSoundManager {
 				.getResourceBundle().getSoundResource(name);
 	}
 	
-	public void playSoundByName(String name, Boolean blocked) {
+	public void playSoundByName(String name, boolean blocked) {
 		this.player.stop();
 		if (this.resources.containsKey(name)) {
 			SoundResource res = (SoundResource) this.resources.get(name);
@@ -103,11 +119,11 @@ public class GameSoundManager {
 		}
 	}
 	
-	public void playMainMenuStartGame(Boolean blocked) {
+	public void playMainMenuStartGame(boolean blocked) {
 		playSoundByName("start-game", blocked);
 	}
 	
-	public void playMainMenuHelp(Boolean blocked) {
+	public void playMainMenuHelp(boolean blocked) {
 		playSoundByName("help", blocked);
 	}
 	
@@ -115,57 +131,116 @@ public class GameSoundManager {
 		playSoundByName("about", blocked);
 	}
 	
-	public void playHelpMenuRules(Boolean blocked) {
+	public void playHelpMenuRules(boolean blocked) {
 		playSoundByName("rules", blocked);
 	}
 	
-	public void playHelpMenuHowToDrawGameBoard(Boolean blocked) {
+	public void playHelpMenuHowToDrawGameBoard(boolean blocked) {
 		playSoundByName("how-to-draw-game-board", blocked);
 	}
 	
-	public void playHelpMenuHowToPlayTheGame(Boolean blocked) {
+	public void playHelpMenuHowToPlayTheGame(boolean blocked) {
 		playSoundByName("how-to-play-the-game", blocked);
 	}
 	
-	public void playDrawFirstVerticalLine(Boolean blocked) {
+	public void playDrawFirstVerticalLine(boolean blocked) {
 		playSoundByName("please-draw-first-vertical-line", blocked);
 	}
 
-	public void playDrawSecondVerticalLine(Boolean blocked) {
+	public void playDrawSecondVerticalLine(boolean blocked) {
 		playSoundByName("please-draw-second-vertical-line", blocked);
 	}
 
-	public void playDrawFirstHorizontalLine(Boolean blocked) {
+	public void playDrawFirstHorizontalLine(boolean blocked) {
 		playSoundByName("please-draw-first-horizontal-line", blocked);
 	}
 
-	public void playDrawSecondHorizontalLine(Boolean blocked) {
+	public void playDrawSecondHorizontalLine(boolean blocked) {
 		playSoundByName("please-draw-second-horizontal-line", blocked);
 	}
 	
-	public void playYourTurn(Boolean blocked) {
+	public void playYourTurn(boolean blocked) {
 		playSoundByName("crosses-is-yours-make-your-turn", blocked);
 	}
 	
-	public void playYouWin(Boolean blocked) {
+	public void playYouWin(boolean blocked) {
 		playSoundByName("congratulations-you-win", blocked);
 	}
 	
-	public void playPenWins(Boolean blocked) {
+	public void playPenWins(boolean blocked) {
 		playSoundByName("sorry-but-pen-wins", blocked);
 	}
 	
-	public void playDraw(Boolean blocked) {
+	public void playDraw(boolean blocked) {
 		playSoundByName("the-game-ends-with-a-draw", blocked);
 	}
 	
-	public void playEasy(Boolean blocked) {
+	public void playEasy(boolean blocked) {
 		playSoundByName("easy", blocked);
 	}
 	
-	public void playHard(Boolean blocked) {
+	public void playHard(boolean blocked) {
 		playSoundByName("hard", blocked);
 	}
+		
+	public void playLineIsToShort(boolean blocked) {
+		playSoundByName("line-is-to-short", blocked);
+	}
 	
+	public void playLineIsNotVertical(boolean blocked) {
+		playSoundByName("line-is-not-vertical", blocked);
+	}
 	
+	public void playLineIsNotHorizontal(boolean blocked) {
+		playSoundByName("line-is-not-horizontal", blocked);
+	}
+	
+	public void playLineMustBeAtTheRight(boolean blocked) {
+		playSoundByName("line-must-be-at-the-right", blocked);
+	}
+	
+	public void playLineMustBeAtTheBottom(boolean blocked) {
+		playSoundByName("line-must-be-at-the-bottom", blocked);
+	}
+	
+	public void playLineMustBeNearOther(boolean blocked) {
+		playSoundByName("line-must-be-near", blocked);
+	}
+	
+	public void playLineMustCross(boolean blocked) {
+		playSoundByName("line-must-cross", blocked);
+	}
+
+	public void playToStartNewGameStartDrawingNewBoard(boolean blocked) {
+		playSoundByName("to-start-new-game-start-drawing", blocked);
+	}
+
+	public void playErrorDrawLine(int reason) {
+		switch (reason) {
+		case GameBoardLineException.REASON_LINE_IS_NOT_HORIZONTAL:
+			playLineIsNotHorizontal(true);
+			break;
+		case GameBoardLineException.REASON_LINE_IS_NOT_VERTICAL:
+			playLineIsNotVertical(true);
+			break;
+		case GameBoardLineException.REASON_LINE_TO_SHORT:
+			playLineIsToShort(true);
+			break;
+		case GameBoardLineException.REASON_MUST_BE_AT_THE_RIGHT:
+			playLineMustBeAtTheRight(true);
+			break;
+		case GameBoardLineException.REASON_MUST_BE_AT_THE_BOTTOM:
+			playLineMustBeAtTheBottom(true);
+			break;
+		case GameBoardLineException.REASON_MUST_BE_NEAR_THE_OTHER_LINES:
+			playLineMustBeNearOther(true);
+			break;
+		case GameBoardLineException.REASON_MUST_CROSS_BOTH_VERTICAL_LINES:
+			playLineMustCross(true);
+			break;
+		default:
+			break;
+		}
+		
+	}
 }
